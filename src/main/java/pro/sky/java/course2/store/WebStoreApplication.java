dd8b0943fd42dcb038381697356d4127dff52175
Vadim <manjavha@gmail.com>
2022-06-03 01:06:37 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/store/WebStoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebStoreApplication.class, args);
+    }
+
+}
